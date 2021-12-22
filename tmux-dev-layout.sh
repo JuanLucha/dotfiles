0b32ea1af07bac179eba9f1f1863942195cb697f
Juan Lucha <juanlucha@gmail.com>
2021-12-22 17:27:59 +0100

    Added matrix effect to the development layout

--- tmux-dev-layout.sh
+++ tmux-dev-layout.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
+# This is a default layout for development environment.
+# The cool matrix effects are achieved using Cmatrix
+# (https://github.com/abishekvashok/cmatrix)
+
 SESSION=development
 
 tmux -2 new-session -d -s $SESSION -x $(tput cols) -y $(tput lines)
 
 tmux new-window -t $SESSION
+tmux split-window -h -l 90%
+tmux send-keys -t 0 "cmatrix -ba" "ENTER"
 tmux split-window -h -l 20%
 tmux split-window -v
-tmux send-keys -t 0 "vim" "ENTER"
+tmux split-window -v -l 20%
+tmux send-keys -t 1 "vim" "ENTER"
+tmux send-keys -t 4 "cmatrix -baC magenta" "ENTER"
 
 # Set default window
 tmux select-window -t $SESSION:1
-tmux select-pane -t 0
+tmux select-pane -t 2
 
 # Attach to session
 tmux -2 attach-session -t $SESSION
