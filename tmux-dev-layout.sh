a483c6a3739b8151917f6fcc83105a83791045f7
Juan Lucha <juanlucha@gmail.com>
2022-01-12 11:05:18 +0100

    Made cmatrix more efficient

--- tmux-dev-layout.sh
+++ tmux-dev-layout.sh
@@ -8,12 +8,12 @@
 
 tmux rename-window dev
 tmux split-window -h -l 90%
-tmux send-keys -t 0 "cmatrix -ba" "ENTER"
+tmux send-keys -t 0 "cmatrix -bau 10" "ENTER"
 tmux split-window -h -l 20%
 tmux split-window -v
 tmux split-window -v -l 20%
 tmux send-keys -t 1 "vim" "ENTER"
-tmux send-keys -t 4 "cmatrix -baC magenta" "ENTER"
+tmux send-keys -t 4 "cmatrix -baC magenta -u 10" "ENTER"
 
 # # Set default window
 tmux select-pane -t 1
--- tmux-note-layout.sh
+++ tmux-note-layout.sh
@@ -17,10 +17,10 @@ fi
 tmux rename-window quests
 tmux move-window -t 9
 tmux split-window -h -l 90%
-tmux send-keys -t 0 "cmatrix -ba" "ENTER"
+tmux send-keys -t 0 "cmatrix -bau 10" "ENTER"
 tmux split-window -h -l 20%
 tmux send-keys -t 1 "vim $questsFile" "ENTER"
-tmux send-keys -t 2 "cmatrix -baC cyan" "ENTER"
+tmux send-keys -t 2 "cmatrix -baC cyan -u 10" "ENTER"
 
 # Set default window
 tmux select-pane -t 1
