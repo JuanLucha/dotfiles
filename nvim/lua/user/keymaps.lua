57ff416c512e7889be1adf27a9865607fa2a6f3b
Juan Lucha <juanlucha@gmail.com>
2022-01-13 11:22:58 +0100

    Added neovim config

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,58 @@
+local opts = { noremap = true, silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Search with space
+keymap("n", "<Space>", "/", opts)
+
+-- Save all buffers at once
+-- keymap("n", "<leader>w", ":wa<CR>", opts)
+
+-- Toggles the explorer
+-- keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Open telescope
+-- keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
+-- keymap("n", "<leader>f", ":Telescope find_files<CR>", opts)
+
+-- Paste without yanking the old text
+keymap("v", "p", '"_dP', opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<S-j>", "<C-d>", opts)
+keymap("n", "<S-k>", "<C-u>", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "fj", "<ESC>", opts)
+
